b05a3f0259342fa80655f71eb2a178169bd3ef21
Fabio Nedrotti <fabio.nedrotti@accenture.com>
2019-01-24 15:29:07 +0100

    Swift example

---
+++ SamplSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamplSwift
+//
+//  Created by a.naidu.jajimoggala on 24/01/19.
+//  Copyright © 2019 a.naidu.jajimoggala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
